fa228c879f14e295d5f257a299e3c1ab384580ee
Thomas Dumont <thomas.dumont@ynov.com>
2019-02-11 16:28:57 +0100

    add mkdir

--- mdtohtml.py
+++ mdtohtml.py
@@ -1,14 +1,14 @@
 from pathlib import *
 from markdown2 import *
-import click
+#import click
 
-#if (Path('../fichiers_html').exists()) = false:
-pathlib.Path("fichiers_html").mkdir()
+if (Path('../Project_MDtoHTML/fichiers_html').exists()) == False:
+    Path('../Project_MDtoHTML/fichiers_html').mkdir()
 
-filesinthefolder = sorted(Path('B:/Bureau/ynov/B1/python/projet_markdown_to_html/fichiers_markdown').glob('*.md'))
+filesinthefolder = sorted(Path('../Project_MDtoHTML/fichiers_markdown').glob('*.md'))
 markdowner = Markdown()
 for filemd in filesinthefolder:
     name_file = PurePosixPath(filemd).stem
-    filehtml = open("B:/Bureau/ynov/B1/python/projet_markdown_to_html/fichiers_html/"+ name_file +".html", "a")
+    filehtml = open("../Project_MDtoHTML/fichiers_html/"+ name_file +".html", "a")
     filehtml.write(markdowner.convert(markdown_path(filemd)))
     filehtml.close()
