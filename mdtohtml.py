27ca60b1aa8ba91b1c63cd5ec3c7f927d4fdf673
Thomas Dumont <thomas.dumont@ynov.com>
2019-02-11 18:57:08 +0100

    Update mdtohtml.py

--- mdtohtml.py
+++ mdtohtml.py
@@ -1,12 +1,12 @@
 from pathlib import *
 from markdown2 import *
-#import click
+
+filesinthefolder = sorted(Path('../Project_MDtoHTML/fichiers_markdown').glob('*.md'))
+markdowner = Markdown()
 
 if (Path('../Project_MDtoHTML/fichiers_html').exists()) == False:
     Path('../Project_MDtoHTML/fichiers_html').mkdir()
 
-filesinthefolder = sorted(Path('../Project_MDtoHTML/fichiers_markdown').glob('*.md'))
-markdowner = Markdown()
 for filemd in filesinthefolder:
     name_file = PurePosixPath(filemd).stem
     filehtml = open("../Project_MDtoHTML/fichiers_html/"+ name_file +".html", "a")
