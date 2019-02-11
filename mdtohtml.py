9b7f305e069a661624d11fad7dc19a28a5c3463d
Thomas Dumont <thomas.dumont@ynov.com>
2019-02-11 15:08:13 +0100

    update

--- mdtohtml.py
+++ mdtohtml.py
@@ -2,6 +2,9 @@ from pathlib import *
 from markdown2 import *
 import click
 
+#if (Path('../fichiers_html').exists()) = false:
+pathlib.Path("fichiers_html").mkdir()
+
 filesinthefolder = sorted(Path('B:/Bureau/ynov/B1/python/projet_markdown_to_html/fichiers_markdown').glob('*.md'))
 markdowner = Markdown()
 for filemd in filesinthefolder:
